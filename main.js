3b9137eab816011252e24c162c0152929a0b2ea7
chldppwls12 <chldppwls12@naver.com>
2020-11-06 02:49:36 +0900

    인포윈도우 추가

--- main.js
+++ main.js
@@ -6,16 +6,23 @@ let container = document.querySelector('#map'); //지도를 담을 영역의 DOM
 
 let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-
-let search_bar = document.querySelector(".search-bar");
-let search_btn = document.querySelector(".search-btn");
+ 
+const search_bar = document.querySelector(".search-bar");
+const search_btn = document.querySelector(".search-btn");
 
 // 장소 검색 객체를 생성합니다
 let ps = new kakao.maps.services.Places(); 
 
-let apiUrl = "https://openapi.gg.go.kr/TninsttInstutM?KEY=9aa60a805f9041198337d6137cd6c761&type=json&pSize=1000&";
-let apiUrl2 = "https://openapi.gg.go.kr/TninsttInstutM?KEY=9aa60a805f9041198337d6137cd6c761&type=json&pIndex=2&pSize=1000&";
-let apiUrl3 = "https://openapi.gg.go.kr/TninsttInstutM?KEY=9aa60a805f9041198337d6137cd6c761&type=json&pIndex=3&pSize=1000&";
+const apiUrl = "https://openapi.gg.go.kr/TninsttInstutM?KEY=9aa60a805f9041198337d6137cd6c761&type=json&pSize=1000&";
+const apiUrl2 = "https://openapi.gg.go.kr/TninsttInstutM?KEY=9aa60a805f9041198337d6137cd6c761&type=json&pIndex=2&pSize=1000&";
+const apiUrl3 = "https://openapi.gg.go.kr/TninsttInstutM?KEY=9aa60a805f9041198337d6137cd6c761&type=json&pIndex=3&pSize=1000&";
+
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+let mapTypeControl = new kakao.maps.MapTypeControl();
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.RIGHT);
+
+
+
 search_bar.addEventListener('keyup', (event) =>{
     if(event.keyCode === 13){
         search_btn.click();
@@ -82,12 +89,12 @@ let getData = async (Lat, Lng) =>{
 
 let drawMaker = (facilityData) => {
     let image = {
-        "math": "./cubes.png",
-        "english": "./abc.png",
-        "music": "./music.png",
-        "art": "./paint.png",
-        "readingRoom": "./reading-book.png",
-        "etc": "./class.png"
+        "math": "./images/cubes.png",
+        "english": "./images/abc.png",
+        "music": "./images/music.png",
+        "art": "./images/paint.png",
+        "essay": "./images/open-book.png",
+        "etc": "./images/class.png"
     }; 
     let imageSize = new kakao.maps.Size(32, 32);
     let imageOption = {offset: new kakao.maps.Point(27, 69)};
@@ -97,17 +104,17 @@ let drawMaker = (facilityData) => {
     
     if((facilityData.CRSE_CLASS_NM && facilityData.CRSE_CLASS_NM.includes("수학")) || facilityData.FACLT_NM && facilityData.FACLT_NM.includes("수학")){
         imageSrc = image.math;
-    }else if((facilityData.CRSE_CLASS_NM && facilityData.CRSE_CLASS_NM.includes("음악")) || facilityData.FACLT_NM && facilityData.FACLT_NM.includes("음악")){
+    }else if((facilityData.CRSE_CLASS_NM && facilityData.CRSE_CLASS_NM.includes("음악", "플룻", "피아노", "첼로")) || facilityData.FACLT_NM && facilityData.FACLT_NM.includes("음악")){
         imageSrc = image.music;
     }
-    else if((facilityData.CRSE_CLASS_NM && facilityData.CRSE_CLASS_NM.includes("영어")) || facilityData.FACLT_NM && facilityData.FACLT_NM.includes("영어")){
+    else if((facilityData.CRSE_CLASS_NM && facilityData.CRSE_CLASS_NM.includes("영어", "외국어")) || facilityData.FACLT_NM && facilityData.FACLT_NM.includes("영어", "어학원", "잉글리쉬")){
         imageSrc = image.english;
     }
     else if((facilityData.CRSE_CLASS_NM && facilityData.CRSE_CLASS_NM.includes("미술")) || facilityData.FACLT_NM && facilityData.FACLT_NM.includes("미술")){
         imageSrc = image.art;
     }   
-    else if(facilityData.CRSE_CLASS_NM === "독서실"){
-        imageSrc = image.readingRoom;
+    else if((facilityData.CRSE_CLASS_NM && facilityData.CRSE_CLASS_NM.includes("논술")) || facilityData.FACLT_NM && facilityData.FACLT_NM.includes("논술")){
+        imageSrc = image.essay;
     }
     else{
         imageSrc = image.etc;
@@ -119,5 +126,21 @@ let drawMaker = (facilityData) => {
         map: map,
         position: new kakao.maps.LatLng(facilityData.REFINE_WGS84_LAT, facilityData.REFINE_WGS84_LOGT),
         image: markerImage,
+        clickable: true,
+    });
+
+    let iwContent = '<div>인포윈도 생성</div>'; // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    const iwPosition = new kakao.maps.LatLng(facilityData.REFINE_WGS84_LAT, facilityData.REFINE_WGS84_LOGT); //인포윈도우 표시 위치입니다
+
+    let infowindow = new kakao.maps.InfoWindow({
+        content : iwContent,
+        removable : true,
+    });
+    
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+          // 마커 위에 인포윈도우를 표시합니다
+          infowindow.open(map, marker);
+          console.log("click")
     });
 }
